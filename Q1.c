25875b01ea7a09c388fc72913992b5c740ee8bb7
Kenneth Robertson <robekenn@oregonstate.edu>
2025-01-10 14:47:43 -0800

    Finished Rec

--- Q1.c
+++ Q1.c
@@ -1,6 +1,6 @@
 /* CS261- Recitation 1 - Q. 1*
- * Name : YOUR Name
- * Recitation time: TIME
+ * Name : Kenneth Robertson
+ * Recitation time: 2:00
  * Solution description: check the properties, like value and address, of pointer
  */
  
@@ -9,21 +9,22 @@
 
 void myFuntion(int* iptr){
      /*Print the value pointed to by iptr*/
-     
+     printf("%i\n", *iptr);
      /*Print the address pointed to by iptr*/
-
+     printf("%p\n", iptr);
      /*Print the address of iptr itself*/
+     printf("%p\n", &iptr);
 }
 
 int main(){
     
     /*declare an integer x and initialize it with 0*/
-    
+    int x = 0;
     /*print the address of x*/
-    
+    printf("%p\n", &x);
     /*Call myFunction() with the address of x*/
-    
+    myFuntion(&x);
     /*print the value of x*/
-
+    printf("%i\n",x);
     return 0;
 }
--- Q2.c
+++ Q2.c
@@ -1,6 +1,6 @@
 /* CS261- Recitation 1 - Q.2
- * Name:  YOUR NAME
- * Recitation time:
+ * Name:  Kenneth Robertson
+ * Recitation time:2:00
  * Complete the code below 
 */
  
@@ -9,14 +9,19 @@
 
 int foo(int a, int* b, int c[], int n){
     /* Set a to double its original value*/
-	
+	a *= 2;
     /* Set b to the maximum value in the array c[] using a for loop */
-	
+	int max = c[0];
+	for(int i = 1; i < n; i++){
+		if(max < c[i])
+			max = c[i];
+	}
+	*b = max;
     /* Set c[0] to triple its original value */
-
+	c[0] *= 3;
 
     /* Return  the sum of a and b */
-
+	return a + *b;
 }
 
 int main(){
@@ -24,16 +29,21 @@ int main(){
     int c[5] = {4,9,1,2,0};
 
     /*Declare two integers x and y and initialize them to 2 and 4 respectively*/
-	
+	int x = 2;
+	int y = 4;
     
     /*Print the values of x, y and the first element of c in one line*/
-
+	printf("x = %i y = %i c[0] = %i\n",x,y, c[0]);
     /*Call foo() appropriately, passing x,y,c,n as parameters*/
+	int ret = foo(x,&y,c,n);
     /*Print the value returned by foo*/
-
+	printf("returned #: %i\n", ret);
     /*Print the values of x, y and the first element of c  again*/
-
-    /*  Did the values printed change? Why? */
+	printf("x = %i y = %i c[0] = %i\n", x, y, c[0]);
+    /*  Did the values printed change? Why?
+     *
+     *	y changed because it is a pointer and not a copy of the variable, the first value of c array changed because it acts as a pointer and we tripled the amount 
+     *  */
     return 0;
 }
     
